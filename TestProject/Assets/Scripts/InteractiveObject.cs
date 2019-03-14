ca8658392bbcccd7efe9a49d20a8bb8d2ceb1f51
Jonathan Way <jonathan.way@loop.colum.edu>
2019-03-14 12:15:47 -0500

    Door video done
    - The door script is made
    - the door frame and the door itself are both made as well
    - the door animation has been created
    - the door animator was made
    - all things related to the door have been hooked up together m=to make a functioning door that opens once.

--- TestProject/Assets/Scripts/InteractiveObject.cs
+++ TestProject/Assets/Scripts/InteractiveObject.cs
@@ -11,7 +11,7 @@ public class InteractiveObject : MonoBehaviour, IInteractive
     public string DisplayText => displayText;
     private AudioSource audioSource;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
